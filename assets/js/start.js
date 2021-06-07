5e86ed48261b17d72ad9d8cb5259cfb3286a9028
Jamie Vesterfelt <jamievesterfelt@hotmail.com>
2021-06-07 13:31:07 -0600

    Worked out bugs saving to localStorage, ready for deployment.

--- assets/js/start.js
+++ assets/js/start.js
@@ -1,6 +1,7 @@
 // Variables
 var apiKey = "31UtlrRos98rGyvqj7WHtq6hfoeMh50h";
 var savedCities = [];
+var tempSavedCities = [];
 
 // Declared Functions
 var getForecast = function(key, cityName) {
@@ -25,7 +26,6 @@ var getForecast = function(key, cityName) {
                     dayHigh.push(forecast.DailyForecasts[i].Temperature.Maximum.Value);
                     dayLow.push(forecast.DailyForecasts[i].Temperature.Minimum.Value);
                 }
-                console.log("getForecast sends to loadForecast");
                 loadForecast(dayPhrase, dayIcon, dayDate, dayHigh, dayLow);
             })
         }
@@ -35,12 +35,9 @@ var getForecast = function(key, cityName) {
 var getCurrentConditions = function(key) {
     var apiUrl = "http://dataservice.accuweather.com/currentconditions/v1/" + key + "?apikey=" + apiKey + "&details=true";
 
-    console.log("apiUrl", apiUrl);
-
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(conditions) {
-                console.log("conditions", conditions);
                 var windSpeed = conditions[0].Wind.Speed.Imperial.Value;
                 var windDirectionDegrees = conditions[0].Wind.Direction.Degrees;
                 var windDirection = conditions[0].Wind.Direction.English;
@@ -65,22 +62,18 @@ var getCurrentConditions = function(key) {
                 switch (uvIndexRating) {
                     case "Low":
                         uvCard.setAttribute("style", "background-color: lightseagreen;");
-                        console.log("low uvindex");
                         break;
 
                     case "Moderate":
                         uvCard.setAttribute("style", "background-color: lightyellow;");
-                        console.log("moderate uvindex");
                         break;
 
                     case "High":
                         uvCard.setAttribute("style", "background-color: #ed8e87;");
-                        console.log("high uvindex");
                         break;
 
                     case "Very High":
                         uvCard.setAttribute("style", "background-color: #fa1d0c;");
-                        console.log("very high uvindex");
                         break;
                 };
 
@@ -96,16 +89,18 @@ var getLocationDetails = function(query) {
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(location) {
-                console.log("location", location);
-                var key = location[0].Key;
-                var cityName = location[0].LocalizedName;
+                if (location[0].Key) {
+                    var key = location[0].Key;
+                    var cityName = location[0].LocalizedName;
 
-                var cityHeader = document.getElementById("city-header");
-                cityHeader.textContent = cityName;
+                    var cityHeader = document.getElementById("city-header");
+                    cityHeader.textContent = cityName;
 
-                console.log("Location key:", key, cityName);
-                getForecast(key, cityName);
-                getCurrentConditions(key);
+                    getForecast(key, cityName);
+                    getCurrentConditions(key);
+                } else {
+                    alert("Location not found. Please verify the zip code.");
+                }
             });
         } else {
             alert("API call failed.");
@@ -114,9 +109,9 @@ var getLocationDetails = function(query) {
 };
 
 var saveSearch = function(query) {
-    getLocationDetails(query);
+    savedCities = tempSavedCities;
 
-    if (!savedCities.includes(query)) {
+    if (savedCities.includes(query)) {} else {
         savedCities.push(query);
     }
 
@@ -124,11 +119,13 @@ var saveSearch = function(query) {
         savedCities.shift();
     }
 
+    getLocationDetails(query);
+
     localStorage.setItem("savedCities", JSON.stringify(savedCities));
 };
 
 var loadCities = function() {
-    var tempSavedCities = JSON.parse(localStorage.getItem("savedCities"));
+    tempSavedCities = JSON.parse(localStorage.getItem("savedCities"));
 
     if (tempSavedCities) {
         var list = document.querySelector(".list-group");
@@ -145,34 +142,83 @@ var loadCities = function() {
 
             listItem.append(listButton);
             list.append(listItem);
-
-            listButton.addEventListener("click", function(event) {
-                event.preventDefault();
-                console.log("city button clicked");
-                getForecast(listButton.textContent);
-
-                for (var i = 0; i < 5; i++) {
-                    var iconImage = document.getElementById("img-" + [i]);
-                    iconImage.setAttribute("src", "");
-
-                    var cardDay = document.getElementById("day-" + [i]);
-                    cardDay.innerHTML = "";
-                }
-            });
         }
 
         var index = tempSavedCities.length - 1;
         getLocationDetails(tempSavedCities[index]);
     } else {
-        console.log("No saved searches.")
+        console.log("No saved searches.");
+        return;
     }
+    var city1 = document.getElementById("city-0");
+    var city2 = document.getElementById("city-1");
+    var city3 = document.getElementById("city-2");
+    var city4 = document.getElementById("city-3");
+    var city5 = document.getElementById("city-4");
+
+    city1.addEventListener("click", function(event) {
+        event.preventDefault();
+        getLocationDetails(city1.textContent);
+
+        for (var i = 0; i < 5; i++) {
+            var iconImage = document.getElementById("img-" + [i]);
+            iconImage.setAttribute("src", "");
+
+            var cardDay = document.getElementById("day-" + [i]);
+            cardDay.innerHTML = "";
+        }
+    });
+    city2.addEventListener("click", function(event) {
+        event.preventDefault();
+        getLocationDetails(city2.textContent);
+
+        for (var i = 0; i < 5; i++) {
+            var iconImage = document.getElementById("img-" + [i]);
+            iconImage.setAttribute("src", "");
+
+            var cardDay = document.getElementById("day-" + [i]);
+            cardDay.innerHTML = "";
+        }
+    });
+    city3.addEventListener("click", function(event) {
+        event.preventDefault();
+        getLocationDetails(city3.textContent);
+
+        for (var i = 0; i < 5; i++) {
+            var iconImage = document.getElementById("img-" + [i]);
+            iconImage.setAttribute("src", "");
+
+            var cardDay = document.getElementById("day-" + [i]);
+            cardDay.innerHTML = "";
+        }
+    });
+    city4.addEventListener("click", function(event) {
+        event.preventDefault();
+        getLocationDetails(city4.textContent);
+
+        for (var i = 0; i < 5; i++) {
+            var iconImage = document.getElementById("img-" + [i]);
+            iconImage.setAttribute("src", "");
+
+            var cardDay = document.getElementById("day-" + [i]);
+            cardDay.innerHTML = "";
+        }
+    });
+    city5.addEventListener("click", function(event) {
+        event.preventDefault();
+        getLocationDetails(city5.textContent);
+
+        for (var i = 0; i < 5; i++) {
+            var iconImage = document.getElementById("img-" + [i]);
+            iconImage.setAttribute("src", "");
+
+            var cardDay = document.getElementById("day-" + [i]);
+            cardDay.innerHTML = "";
+        }
+    });
 };
 
-var loadLastCity = function(savedCities) {};
-
 var loadForecast = function(dayPhrase, dayIcon, dayDate, dayHigh, dayLow) {
-    var card = document.querySelector(".card");
-
     for (var i = 0; i < 5; i++) {
         var iconImage = document.getElementById("img-" + [i]);
         iconImage.setAttribute("src", "./assets/images/" + dayIcon[i] + ".png");
@@ -202,11 +248,13 @@ var loadForecast = function(dayPhrase, dayIcon, dayDate, dayHigh, dayLow) {
 };
 
 var getWeather = function() {
-
     var submitBtn = document.querySelector("#search-btn");
+
     submitBtn.addEventListener("click", function(event) {
         event.preventDefault();
+
         var query = document.getElementById("search-text").value;
+
         saveSearch(query);
 
         for (var i = 0; i < 5; i++) {
@@ -216,8 +264,9 @@ var getWeather = function() {
             var cardDay = document.getElementById("day-" + [i]);
             cardDay.innerHTML = "";
         }
-        var searchText = document.getElementById("search-text");
-        searchText.textContent = "";
+
+        var inputText = document.getElementById("search-text");
+        inputText.value = "";
     });
 
     loadCities();
